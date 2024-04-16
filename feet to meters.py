72143ab5c6c0ea462a3031589c0ef9c3d5be39c5
Tarequl Islam Mukul <882737@gmail.com>
2024-04-16 23:52:53 +0600

    Update feet to meters.py

--- feet to meters.py
+++ feet to meters.py
@@ -1,28 +1,30 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
+from tkinter.constants import N, W, E, S
 
 def calculate(*args):
     try:
-        value=float(feet.get())
-        meters.set(int(0.3048*value *10000.0 + 0.5)/10000.0)
+        value = float(feet.get())
+        meters.set(round(0.3048 * value, 4))
     except ValueError:
-        pass
-root= Tk()
+        meters.set("Invalid input")
+
+root = tk.Tk()
 root.title("Feet to meters")
 
-mainframe=ttk.Frame(root,padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
-root.columnconfigure(0,weight=1)
-root.rowconfigure(0,weight=1)
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-feet = StringVar()
+feet = tk.StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
 
-meters =StringVar()
-ttk.Label(mainframe,textvariable=meters).grid(column=2,row=2,sticky=(W,E))
+meters = tk.StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
-ttk.Button(mainframe,text=calculate,command=calculate).grid(column=3,row=3,sticky=W)
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
 
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
@@ -32,6 +34,5 @@ for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
 
 feet_entry.focus()
-root.bind("<Return>", calculate)
 
 root.mainloop()
